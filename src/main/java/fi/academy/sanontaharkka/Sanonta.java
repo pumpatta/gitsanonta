c9682739f1c7d2ce3f8eb814ec827e1827978fce
pumpatta <jliljavirta@gmail.com>
2018-03-01 10:54:42 +0200

    Sanonta-luokka getterit ja setterit

--- src/main/java/fi/academy/sanontaharkka/Sanonta.java
+++ src/main/java/fi/academy/sanontaharkka/Sanonta.java
@@ -1,4 +1,45 @@
 package fi.academy.sanontaharkka;
 
 public class Sanonta {
+
+    private String teksti;
+    private String kertoja;
+    private int id;
+
+    public Sanonta() {
+    }
+
+    public Sanonta(String teksti) {
+        this.teksti = teksti;
+    }
+
+    public Sanonta(String teksti, String kertoja) {
+        this.teksti = teksti;
+        this.kertoja = kertoja;
+        this.id = id; //id:n pitää päivittyä automaattisesti
+    }
+
+    public String getTeksti() {
+        return teksti;
+    }
+
+    public void setTeksti(String teksti) {
+        this.teksti = teksti;
+    }
+
+    public String getKertoja() {
+        return kertoja;
+    }
+
+    public void setKertoja(String kertoja) {
+        this.kertoja = kertoja;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
