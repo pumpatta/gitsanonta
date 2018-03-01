a60407c5fbd9fb03745d04d4e2d9484cedaeddb0
pumpatta <jliljavirta@gmail.com>
2018-03-01 10:24:43 +0200

    alkusetti

---
+++ src/main/java/fi/academy/sanontaharkka/SanontaharkkaApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.sanontaharkka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanontaharkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanontaharkkaApplication.class, args);
+	}
+}
