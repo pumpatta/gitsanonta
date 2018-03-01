72fcce6315040d87800e3e2af03599dd8f92e770
hdnurmin <heidi.a.nurminen@gmail.com>
2018-03-01 11:03:54 +0200

    Kontrollerin luonti

--- src/main/java/fi/academy/sanontaharkka/Kontrolleri.java
+++ src/main/java/fi/academy/sanontaharkka/Kontrolleri.java
@@ -1,11 +1,18 @@
 package fi.academy.sanontaharkka;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class Kontrolleri {
-
+    @GetMapping ("/sanontalista")
+    public String sanonnatSivulle (Model model) {
+        Sanonta sanonta1 = new Sanonta("Elämä on laiffii", "Matti Nykänen");
+        model.addAttribute("thViite", sanonta1);
+        return "etusivu";
+    }
 
 
 }
