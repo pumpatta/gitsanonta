9634f4df40ce04512fd801b1fb96f9c648173a57
joniketo <joni.m.keto@gmail.com>
2018-03-01 11:11:59 +0200

    joni muokkaili etusivua

--- src/main/java/fi/academy/sanontaharkka/Kontrolleri.java
+++ src/main/java/fi/academy/sanontaharkka/Kontrolleri.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 
 public class Kontrolleri {
-    @GetMapping ("/sanontalista")
+    @GetMapping ("/etusivu")
     public String sanonnatSivulle (Model model) {
         Sanonta sanonta1 = new Sanonta("Elämä on laiffii", "Matti Nykänen");
         model.addAttribute("thViite", sanonta1);
